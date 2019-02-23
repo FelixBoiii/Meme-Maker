23eae7ae68e57a7fb063a24df2c1c84665529c35
FelixBoiii <47904281+FelixBoiii@users.noreply.github.com>
2019-02-23 21:54:36 +0100

    more javascript for init();

--- main.js
+++ main.js
@@ -12,7 +12,21 @@ function memeGenerator(img, topText, bottomText, topTextSize, bottomTextSize){
 }
 
 function init(){
-  topTextInput = document.getElementById('top-text')
+  topTextInput = document.getElementById('top-text');
+  bottomTextInput = document.getElementById('bottom-text');
+  topTextSizeInput = document.getElementById('top-text-size-input');
+  bottomTextSizeInput = document.getElementById('bottom-text-size-input');
+  imageInput = document.getElementById('image-input');
+  generateBtn = document.getElementById('generate-btn');
+  canvas = document.getElementById('meme-canvas');
+
+  ctx = canvas.getContext('2d');
+
+  canvas.width = canvas.height = 0;
+
+  //default text
+  topTextInput.value = bottomTextInput.value = 'Demo\nText';
+
 }
 
 init();
