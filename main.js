778811cd5cd593f3053c124342c762db6100ec79
FelixBoiii <47904281+FelixBoiii@users.noreply.github.com>
2019-02-23 21:46:34 +0100

    new js

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-function memeGenerator(){
-  
+let topTextInput, bottomTextInput, topTextSizeInput, bottomTextSizeInput, imageInput, generateBtn, canvas, ctx;
+
+function memeGenerator(img, topText, bottomText, topTextSize, bottomTextSize){
+  let fontSize;
+
+  // size canvas to image
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  //clears canvas
+  ctx.clearRect(0,0, canvas.width, canvas.height)
 }
 
 function init(){
-
+  topTextInput = document.getElementById('top-text')
 }
 
 init();
