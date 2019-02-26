6c8e7cdae614136b97b941908b48918d0abbd201
FelixBoiii <47904281+FelixBoiii@users.noreply.github.com>
2019-02-26 15:45:56 +0100

    load image as soon as its put in

--- main.js
+++ main.js
@@ -1,48 +1,42 @@
 let topTextInput, bottomTextInput, topTextSizeInput, bottomTextSizeInput, imageInput, generateBtn, canvas, ctx;
 
-function generateMeme (img, topText, bottomText, topTextSize, bottomTextSize) {
+function generateMeme(img, topText, bottomText, topTextSize, bottomTextSize) {
     let fontSize;
-
     // Size canvas to image
     canvas.width = img.width;
     canvas.height = img.height;
-
     // Clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     // Draw main image
     ctx.drawImage(img, 0, 0);
-
+    //img.onload = () => ctx.drawImage(img, 0, 0);
     // Text style: white with black borders
     ctx.fillStyle = 'white';
     ctx.strokeStyle = 'black';
     ctx.textAlign = 'center';
-
     // Top text font size
     fontSize = canvas.width * topTextSize;
     ctx.font = fontSize + 'px Impact';
     ctx.lineWidth = fontSize / 30;
-
     // Draw top text
     ctx.textBaseline = 'top';
-    topText.split('\n').forEach(function (t, i) {
+    topText.split('\n').forEach(function(t, i) {
         ctx.fillText(t, canvas.width / 2, i * fontSize, canvas.width);
         ctx.strokeText(t, canvas.width / 2, i * fontSize, canvas.width);
     });
-
     // Bottom text font size
     fontSize = canvas.width * bottomTextSize;
     ctx.font = fontSize + 'px Impact';
     ctx.lineWidth = fontSize / 30;
-
     // Draw bottom text
     ctx.textBaseline = 'bottom';
-    bottomText.split('\n').reverse().forEach(function (t, i) { // .reverse() because it's drawing the bottom text from the bottom up
+    bottomText.split('\n').reverse().forEach(function(t, i) { // .reverse() because it's drawing the bottom text from the bottom up
         ctx.fillText(t, canvas.width / 2, canvas.height - i * fontSize, canvas.width);
         ctx.strokeText(t, canvas.width / 2, canvas.height - i * fontSize, canvas.width);
     });
 }
 
-function init () {
+function init() {
     // Initialize variables
     topTextInput = document.getElementById('top-text');
     bottomTextInput = document.getElementById('bottom-text');
@@ -51,17 +45,17 @@ function init () {
     generateBtn = document.getElementById('generate-btn');
     canvas = document.getElementById('meme-canvas');
     imageInput = document.getElementById('image-input');
-
+    imageInput.addEventListener('change', handleImage, false);
     ctx = canvas.getContext('2d');
     canvas.width = canvas.height = 0;
 
     // Default/Demo text
     topTextInput.value = bottomTextInput.value = 'Demo\nText';
     // Generate button click listener
-    generateBtn.addEventListener('click', function () {
+    generateBtn.addEventListener('click', function() {
         // Read image as DataURL using the FileReader API
         let reader = new FileReader();
-        reader.onload = function () {
+        reader.onload = function() {
             let img = new Image;
             img.src = reader.result;
             generateMeme(img, topTextInput.value, bottomTextInput.value, topTextSizeInput.value, bottomTextSizeInput.value);
@@ -70,36 +64,48 @@ function init () {
     });
 }
 
-
-function download_image(){
-  var canvas = document.getElementById("meme-canvas");
-  image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-  var link = document.createElement('a');
-  link.download = "meme.png";
-  link.href = image;
-  link.click();
+function handleImage(e){
+    var reader = new FileReader();
+    reader.onload = function(event){
+        var img = new Image();
+        img.onload = function(){
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img,0,0);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);
 }
 
-function download_image2(){
+
+function download_image2() {
     var canvas = document.getElementById("meme-canvas");
     var link = document.createElement("a");
-      var imgData = canvas.toDataURL({    format: 'png',
-        multiplier: 4});
-      var strDataURI = imgData.substr(22, imgData.length);
-      var blob = dataURLtoBlob(imgData);
-      var objurl = URL.createObjectURL(blob);
-      link.download = "meme.png";
-      link.href = objurl;
-     link.click();
+    var imgData = canvas.toDataURL({
+        format: 'png',
+        multiplier: 4
+    });
+    var strDataURI = imgData.substr(22, imgData.length);
+    var blob = dataURLtoBlob(imgData);
+    var objurl = URL.createObjectURL(blob);
+    link.download = "meme.png";
+    link.href = objurl;
+    link.click();
 }
 
 function dataURLtoBlob(dataurl) {
-    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-    while(n--){
+    var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+    while (n--) {
         u8arr[n] = bstr.charCodeAt(n);
     }
-    return new Blob([u8arr], {type:mime});
+    return new Blob([u8arr], {
+        type: mime
+    });
 }
 
 init();
